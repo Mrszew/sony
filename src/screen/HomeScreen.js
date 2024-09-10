e353e24645516581b7192869bb6aa3027b91077e
Mrszew <marcelraszewski@gmail.com>
2024-09-11 00:08:43 +0200

    new

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+
 export default function HomeScreen() {
   const navigation = useNavigation()
 
@@ -14,17 +15,17 @@ export default function HomeScreen() {
   return (
     <SafeAreaView>
       <View>
-        <Text>helo</Text>
-        <Image style={style.image} source={{uri:"https://cdn.aniagotuje.com/pictures/articles/2023/02/38768540-v-1080x1080.jpg"}} resizeMode='contain'/>
+        <Text className='text-red-500'>hello</Text>
+        <Image source={{uri:"https://cdn.aniagotuje.com/pictures/articles/2023/02/38768540-v-1080x1080.jpg"}} resizeMode='contain'/>
       </View>
       </SafeAreaView>
   );
 }
 
-const style = StyleSheet.create({
-  image:{
-    padding: 5,
-    aspectRatio:1,
-    maxWidth:100
-  }
-})
+// const style = StyleSheet.create({
+//   image:{
+//     padding: 5,
+//     aspectRatio:1,
+//     maxWidth:100
+//   }
+// })
