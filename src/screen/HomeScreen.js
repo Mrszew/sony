15612861e523fd9df5e81fe88867069ddb55093b
Mrszew <marcelraszewski@gmail.com>
2024-09-10 23:57:14 +0200

    new

---
+++ src/screen/HomeScreen.js
@@ -0,0 +1,30 @@
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+export default function HomeScreen() {
+  const navigation = useNavigation()
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+    headerShown:false
+    },[])
+  })
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>helo</Text>
+        <Image style={style.image} source={{uri:"https://cdn.aniagotuje.com/pictures/articles/2023/02/38768540-v-1080x1080.jpg"}} resizeMode='contain'/>
+      </View>
+      </SafeAreaView>
+  );
+}
+
+const style = StyleSheet.create({
+  image:{
+    padding: 5,
+    aspectRatio:1,
+    maxWidth:100
+  }
+})
